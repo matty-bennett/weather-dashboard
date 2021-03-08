9356f38d6bd981cf13c964d37000230d6d48354c
matty-bennett <bennyot10@gmail.com>
2021-03-07 22:08:27 -0700

    added dates with moment and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# weather-dashboard
+# Weather Dashboard
+When a user enters a city name into the search bar, they are presented with that city's current weather conditions as well as a forecast for the next five days. The eight most recently searched cities will be added to a list beneath the search bar where they can be selected again.
+
+<img src="./assets/weather-dashboard-ss.png"/>
+link: https://matty-bennett.github.io/weather-dashboard/
--- assets/main.js
+++ assets/main.js
@@ -14,7 +14,7 @@ $("#search-btn").click(function(event) {
     getForecast();
     getCurrentWeather();
     // append searches to 
-    
+
 
 })
 
@@ -25,6 +25,12 @@ let getCurrentWeather = () => {
     })
     .then(function(data) {
         // current weather info
+        let currentDateEl = $('#current-date');
+        let currentDate = moment().format('MM/DD/YYYY');
+        currentDateEl.append(currentDate);
+
+        
+    
         let currentTempEl = $('#current-temp');
         let currentTemp = data.main.temp;
         currentTempEl.append(currentTemp + '°F');
@@ -72,20 +78,28 @@ let getForecast = () => {
     })
     .then(function(data) {
         // day one
+        let dateOneEl = $('#date-one');
+        let dateOne = moment().add(1, 'd').format('MM/DD/YYYY');
+        dateOneEl.append(dateOne);
+
         let dayOneTempEl = $('#day-one-temp');
         let dayOneTemp = data.list[3].main.temp;
         dayOneTempEl.append(Math.floor(dayOneTemp) + '°F');
 
         let dayOneIconEl = $('#day-one-icon');
-        let dayOneIcon = data.list[3].weather[0].icon;
-        dayOneIconEl.src = 'http://openweathermap.org/img/wn/' + dayOneIcon + '@2x.png';
-        console.log(dayOneIcon);
+        dayOneIconEl.src = 'http://openweathermap.org/img/wn/' + data.list[3].weather[0].icon + '@2x.png';
+        dayOneIconEl.className = 'icon';
+        
 
         let dayOneHumidEl = $('#day-one-humid');
         let DayOneHumid = data.list[3].main.humidity;
         dayOneHumidEl.append(DayOneHumid + '%');
 
         // day two
+        let dateTwoEl = $('#date-two');
+        let dateTwo = moment().add(2, 'd').format('MM/DD/YYYY');
+        dateTwoEl.append(dateTwo);
+
         let dayTwoTempEl = $('#day-two-temp');
         let dayTwoTemp = data.list[11].main.temp;
         dayTwoTempEl.append(Math.floor(dayTwoTemp) + '°F');
@@ -100,6 +114,10 @@ let getForecast = () => {
         dayTwoHumidEl.append(DayTwoHumid + '%');
 
         // day three
+        let dateThreeEl = $('#date-three');
+        let dateThree = moment().add(3, 'd').format('MM/DD/YYYY');
+        dateThreeEl.append(dateThree);
+
         let dayThreeTempEl = $('#day-three-temp');
         let dayThreeTemp = data.list[19].main.temp;
         dayThreeTempEl.append(Math.floor(dayThreeTemp) + '°F');
@@ -114,6 +132,10 @@ let getForecast = () => {
         dayThreeHumidEl.append(DayThreeHumid + '%');
 
         // day 4
+        let dateFourEl = $('#date-four');
+        let dateFour = moment().add(4, 'd').format('MM/DD/YYYY');
+        dateFourEl.append(dateFour);
+
         let dayFourTempEl = $('#day-four-temp');
         let dayFourTemp = data.list[27].main.temp;
         dayFourTempEl.append(Math.floor(dayFourTemp) + '°F');
@@ -128,6 +150,10 @@ let getForecast = () => {
         dayFourHumidEl.append(DayFourHumid + '%');
 
         // day 5
+        let dateFiveEl = $('#date-five');
+        let dateFive = moment().add(5, 'd').format('MM/DD/YYYY');
+        dateFiveEl.append(dateFive);
+
         let dayFiveTempEl = $('#day-five-temp');
         let dayFiveTemp = data.list[35].main.temp;
         dayFiveTempEl.append(Math.floor(dayFiveTemp) + '°F');
